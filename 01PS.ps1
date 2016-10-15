c4ab32a108a23dd9d7881c10578675c4c188bf28
1Liol <hxc@yandex.ru>
2016-10-15 17:06:36 +0300

    First function in prachtice script

--- 01PS.ps1
+++ 01PS.ps1
@@ -1,7 +1,23 @@
-function GetString([String[]]$serviceName) {
+function PutSTR() {
+
+    Param(
+    [String[]]$serviceName,
+    [String[]]$running
+    )
    
-    foreach ($i in $serviceName)
+    foreach ($i in $serviceName){
     
-    {Write-Output  $i >> C:\scripts\test.txt}
-   
-}
+    Write-Output  $i >> C:\scripts\test.txt
+
+    foreach ($a in Get-Content C:\scripts\test.txt){
+
+    
+
+    if (Get-Service $a -ErrorAction SilentlyContinue ) {
+     
+     Get-Service -Name $a | Group-Object Status 
+    
+    }
+   }
+  }
+ }
