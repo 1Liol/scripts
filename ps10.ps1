5537e93ee7f3a8de802b70612aca7cfdb2cc626d
1Liol <hxc@yandex.ru>
2016-12-27 11:02:38 +0300

    yap

---
+++ ps10.ps1
@@ -0,0 +1,13 @@
+﻿<#
+1.	Create a file that will ping remote server and write results to 
+<System Disk>:\ping.txt.
+#>
+
+$file = 'ping.txt'
+$ping = '8.8.8.8'
+if (-not(Test-Path $file)){
+New-Item -Path $env:SystemDrive$file -ItemType File -Force | ping $ping | Out-File $file
+}
+else{
+ping 8.8.8.8 | Out-File $env:SystemDrive$file
+}
---
+++ ps11.ps1
@@ -0,0 +1,9 @@
+﻿param (
+[string]$SourcePath,
+[string]$DistPath
+)
+gci . -File | Select-Object -Property Name | Out-File .\file.txt
+$a = Get-Content .\file.txt | where {$_.name}a
+
+Write-Output .\file.txt
+
