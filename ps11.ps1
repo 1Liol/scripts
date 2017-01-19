6eea397fd6744546d04ec62d7c1f2d98da5e5da9
1Liol <hxc@yandex.ru>
2017-01-19 03:04:19 +0300

    poor shit

--- ps11.ps1
+++ ps11.ps1
@@ -1,9 +1,16 @@
-﻿param (
-[string]$SourcePath,
-[string]$DistPath
+﻿<#
+11.	You have file “files.txt” that contains list of required
+files (each file name on new line). You need to copy files from
+this list from specified folder to another folder.
+#>
+param(
+
+[Parameter(Mandatory=$true,ValueFromPipeline = $true)]
+[string]$file,
+[string]$dest
 )
-gci . -File | Select-Object -Property Name | Out-File .\file.txt
-$a = Get-Content .\file.txt | where {$_.name}a
 
-Write-Output .\file.txt
 
+foreach($line in Get-Content $file) {
+Copy-Item  $line -Destination $dest
+}
