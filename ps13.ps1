04e1c7c29ad5065b532d8ef66eacfe820447cc58
1Liol <hxc@yandex.ru>
2017-01-22 04:09:37 +0300

    yet another shitty scripts

---
+++ ps13.ps1
@@ -0,0 +1,12 @@
+﻿<#
+ # 12.	You have two folders. You need to compare files in them and write files names that for the files that are identical to “files.txt” file
+#>
+
+
+Param(
+[parameter(Mandatory=$true)]
+[string]$Source
+
+)
+New-Item -Path . -Name Files.txt
+Get-ChildItem -Path $Source -Recurse | Select-Object Name | Out-File Files.txt 
---
+++ ps14.ps1
@@ -0,0 +1,22 @@
+﻿<#
+13.	You have text file. You need to find all lines in it that contain word “QA” and write them to a file “QA.txt”
+#>
+function Get-QA
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                  Position=0)]
+                  $Source
+    )
+
+    Begin
+    {
+    Get-Content $Source | Select-String -Pattern "QA" > StrContQA.txt
+    }
+}
